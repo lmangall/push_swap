f2b1c0fb86529190b079ce405dde94918b8f0d82
Leonard Mangallon <lmangall@c4c2c3.42berlin.de>
2023-07-05 22:37:07 +0200

    correct small errors

--- src/push_swap.c
+++ src/push_swap.c
@@ -24,10 +24,10 @@ void	stack_ini(t_list **stack, int argc, char **argv)
 	if (argc == 2)
 		args = ft_split(argv[1], ' ');
 	else
-		args = argv;
-	while (args[i])//before was:   while (i < argc)
+		args = &argv[1];
+	while (args[i])
 	{
-		check_int(args[i]); // Validate the integer before adding it to the stack
+		check_int(args[i]);
 		new_list = ft_lstnew(atoi(args[i]));
 		ft_lstadd_back(stack, new_list);
 		i++;
@@ -41,20 +41,16 @@ int	main(int argc, char **argv)
 	t_list	**stack_a;
 	t_list	**stack_b;
 
-	//  The reason for using double pointers
-	// is to allow modifications to the original pointers within a function.
 	if (argc < 2)
 		exit_free("Nothing to sort", NULL, NULL);
-	// ft_check_args(argc, argv);
 	stack_a = (t_list **)malloc(sizeof(t_list));
 	stack_b = (t_list **)malloc(sizeof(t_list));
-	// NULL signifies the end of the list. => those are empty
 	*stack_a = NULL;
 	*stack_b = NULL;
 	stack_ini(stack_a, argc, argv);
 	assign_index_by_value(*stack_a);
 	algo(stack_a, stack_b);
-	print_list(*stack_a);
+	// print_list(*stack_a);
 	exit_free("allgood", *stack_a, *stack_b);
 	return (0);
 }
